dfaaf05a6f3380e2e798ee44c0456054a7a618f8
obinnaakaolisa <akaolisaobinna@gmail.com>
2022-10-27 16:46:20 +0100

    Added week 4 files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,1 @@
-const loginBtn = document.getElementById('loginBtn');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const form = document.getElementById('loginForm');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-});
-
-loginBtn.addEventListener('click', () => {
-    if(email != '' && password !='') {
-        window.location.href='/success.html';
-    }
-});
+//
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # Assignments
-> Week 3 tasks for Frontend Zuri training, cohort 2
+> Week 4 tasks for Frontend Zuri training, cohort 2
 
-## Week 3
-Using HTML and CSS
+## Week 4
+CSS continued
+
+ 
 
